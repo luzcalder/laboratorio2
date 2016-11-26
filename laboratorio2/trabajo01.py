0513a62ef8b9024ea44af877c04d728291eb2600
luzcalder <botaniczul@gmail.com>
2016-11-25 20:45:55 -0600

    Add files via upload

---
+++ laboratorio2/trabajo01.py
@@ -0,0 +1,13 @@
+#funcion
+#triangulo rectangulo
+#luz maria calderon aguilera
+entrada=input("catetos de untriangulo rectangulo ")
+a= input()
+b= input()
+def suma(a,b):
+    sa=a+b
+    print sa
+
+
+salida=suma(entrada)
+print salida
---
+++ laboratorio2/trabajo1.py
@@ -0,0 +1,15 @@
+#tarea 2
+#luz maria calderon aguilera
+#tarifa
+def funcion(x):
+    entrada= input("distancia recorridos")
+    tarifa=0
+    banderazo=8.74
+    distancia=0
+    tarifaf=1.84*4
+    kpesos=x*tarifaf+banderazo
+    print funcion(x)
+
+
+    salida=funcion(entrada)
+    print salida
