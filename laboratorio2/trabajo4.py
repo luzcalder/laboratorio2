822ed8d68ca101c20947542bbd5ce9674aa4a535
luzcalder <botaniczul@gmail.com>
2016-11-25 21:07:48 -0600

    Add files via upload

---
+++ laboratorio2/trabajo4.py
@@ -0,0 +1,8 @@
+#tarea
+#luz maria claderon aguilera
+import matplotlib.pyplot as plt
+def funcion(x):
+    lista=[[2,3],[4,0],[3,-3],[1,-3],[-1,0],[2,3]]
+    plt.plot(lista)
+    plt.show()
+    print funcion
